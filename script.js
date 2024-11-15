48f0de61cc360dcd9957f9d40a9b51a2978a7cd0
Piquerez09 <joachim.andradeimbru@gmail.com>
2024-11-15 12:35:14 -0300

    j

---
+++ script.js
@@ -0,0 +1,6 @@
+// Aqui podemos adicionar funcionalidades JavaScript para tornar o site mais interativo no futuro
+// Como animações, gráficos ou interações com formulários.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Site carregado com sucesso!");
+});
