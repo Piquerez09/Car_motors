367b3cf3223e7cc24e196fc7b7bd6e3b41b4accb
Piquerez09 <joachim.andradeimbru@gmail.com>
2024-11-16 12:39:21 -0300

    j

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-// Aqui podemos adicionar funcionalidades JavaScript para tornar o site mais interativo no futuro
-// Como animações, gráficos ou interações com formulários.
-
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Site carregado com sucesso!");
+    const ctx = document.getElementById('comparativoGrafico').getContext('2d');
+    const comparativoGrafico = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['V4', 'V6', 'V8', 'V10', 'V12'],
+            datasets: [{
+                label: 'Potência (cv)',
+                data: [150, 300, 500, 600, 800],
+                backgroundColor: ['#3498db', '#e74c3c', '#2ecc71', '#f39c12', '#9b59b6'],
+                borderColor: ['#2980b9', '#c0392b', '#27ae60', '#f1c40f', '#8e44ad'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Comparativo de Potência entre os Motores'
+                }
+            }
+        });
 });
